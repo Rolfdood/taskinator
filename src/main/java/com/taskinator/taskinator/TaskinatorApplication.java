a4a7ace438cc4ac02188b814889eb535c41192da
Rolfdood <ralphclarencebello@gmail.com>
2026-05-30 23:30:40 +0800

    project setup

---
+++ src/main/java/com/taskinator/taskinator/TaskinatorApplication.java
@@ -0,0 +1,13 @@
+package com.taskinator.taskinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskinator/taskinator/TaskinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskinator.taskinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
